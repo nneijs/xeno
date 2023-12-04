25a4aaabd9584f4a45e7c1cf2afbce8d7d28f206
nnei <kektheboi@gmail.com>
2023-12-04 21:17:14 +0100

    updated the readme and added license

--- xeno.ts
+++ xeno.ts
@@ -1,5 +1,4 @@
-// my-ts-ui-library.ts
-namespace xeno {
+namespace xenopackage {
     export interface ButtonProps {
         label: string;
         onClick: () => void;
@@ -10,6 +9,7 @@ namespace xeno {
 
         const buttonElement = document.createElement('button');
         buttonElement.innerText = label;
+        buttonElement.classList.add('my-button'); // Apply the style class
         buttonElement.addEventListener('click', onClick);
 
         return buttonElement;
@@ -17,11 +17,11 @@ namespace xeno {
 
     export function mount(component: HTMLElement, containerId: string): void {
         const container = document.getElementById(containerId);
-    
+
         if (container) {
             container.appendChild(component);
         } else {
             console.error(`Container with id '${containerId}' not found.`);
         }
     }
-}    
+}
